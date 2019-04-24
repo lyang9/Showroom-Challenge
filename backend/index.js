7ac097f18a04ecfaa33d92b2fd32d80dd14474f1
Linda Yang <yanglinda24@gmail.com>
2019-04-23 22:07:40 -0400

    Imported node dependencies and middleware. Listening port. Used sanity check

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+// node dependencies
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+// middleware
+const server = express();
+
+server.use(express.json());
+server.use(helmet(), cors());
+
+// sanity check
+server.get('/', (req, res) => {
+  res.send('It is working!');
+});
+
+// listening port
+const port = 5000;
+server.listen(port, function() {
+  console.log(`\n=== API listening on port ${port} ===\n`);
+});
