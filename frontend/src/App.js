e46cc376aca0b119d6040a49e956423b389660e7
Linda Yang <yanglinda24@gmail.com>
2019-04-27 01:19:14 -0400

    Changed function to class

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 
-function App() {
-  return (
-    <React.Fragment>
-      <Navbar />
-    </React.Fragment>
-  );
+class App extends Component () {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
