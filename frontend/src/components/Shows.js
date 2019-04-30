abba3f6e587b3939a206ae0f380b275eee04ce23
Linda Yang <yanglinda24@gmail.com>
2019-04-29 23:40:26 -0400

    Created shows component

---
+++ frontend/src/components/Shows.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+
+};
+
+class Shows extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      shows: [],
+      title: '',
+      username: '',
+    }
+  }
+
+  componentDidMount() {
+    axios.get('')
+      .then(response => this.setState({ shows: response.data }))
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <div className={classes.root}>
+        <Typography variant='h3' gutterBottom>
+          Masterlist of TV Shows
+        </Typography>
+        <Typography variant='h5' gutterBottom>
+          {this.state.title}
+        </Typography>
+        <Typography variant='h5' gutterBottom>
+          Who's Watching? {this.state.username}
+        </Typography>
+      </div>
+    );
+  }
+}
+
+Shows.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Shows);
