044cb267e79a166789ee8d5025366ea66298deb8
Linda Yang <yanglinda24@gmail.com>
2019-04-29 23:37:09 -0400

    Imported UserWatching, UserPost, Shows. Rendered route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,9 @@ import { Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Users from './components/Users';
+import UserWatching from './components/UserWatching';
+import UserPost from './components/UserPost';
+import Shows from './components/Shows';
 
 class App extends Component {
   render() {
@@ -15,6 +18,9 @@ class App extends Component {
         <Switch>
           <Route exact path='/' component={Home}></Route>
           <Route path='/users' component={Users}></Route>
+          <Route path='/user/:id' component={UserWatching}></Route>
+          <Route path='/user/post' component={UserPost}></Route>
+          <Route path='/shows' component={Shows}></Route>
         </Switch>
       </React.Fragment>
     );
