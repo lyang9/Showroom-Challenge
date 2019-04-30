adb6836410ecb0ae5180d00923885c6eb9549da9
Linda Yang <yanglinda24@gmail.com>
2019-04-29 23:39:21 -0400

    Comments

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import logo from '../tvlogo.png';
+// import { Link } from 'react-router-dom';
+// import logo from '../tvlogo.png';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
+// import Button from '@material-ui/core/Button';
 
 const styles = {
   root: {
